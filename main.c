c7806008b470dc36c6d09faff80ea88978c4b20e
Ahmed Osama Metwally <95638384+AhmedOsama21998@users.noreply.github.com>
2023-04-26 18:48:19 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,15 +11,10 @@
 
 int main(void)
 {
-	DIO_enuInit();/*
-u8 CathodSeg[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
 DIO_enuInit();
-	while (1)
+
+while (1)
 	{
-		for(int i=0;i<=9;i++)
-		{
-		DIO_enuSetPortValue(DIO_u8PORTA,CathodSeg[i]);
-		_delay_ms(500);
-		}
-	}*/
+
+	}
 }
